738b984ab9d6bdd7f7530dfe59204689c8536b47
Fraser <frazemor@hotmail.com>
2022-06-18 15:51:08 -0400

    Set to deploy mode

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -22,10 +22,10 @@ import {
 
 
 //Dev mode
-const serverURL = ""; //enable for dev mode
+//const serverURL = ""; //enable for dev mode
 
 //Deployment mode instructions
-//const serverURL = "http://ov-research-4.uwaterloo.ca:3040"; //enable for deployed mode; Change PORT to the port number given to you;
+const serverURL = "http://ov-research-4.uwaterloo.ca:3040"; //enable for deployed mode; Change PORT to the port number given to you;
 //To find your port number: 
 //ssh to ov-research-4.uwaterloo.ca and run the following command: 
 //env | grep "PORT"
